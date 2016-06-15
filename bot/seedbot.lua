7a90639679e0306d86740c2a597230f5b84895ab
hoseinsh69 <hosein.shahravi69.hs@gmail.com>
2016-06-15 15:23:48 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {spam69_bot},--Sudo users
+    sudo_users = {hoseinsh69},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
